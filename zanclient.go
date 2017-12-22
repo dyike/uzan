9b5dec0eeae7c9b03d35227720acf892f64c90c0
ityike <yuanfeng634@gmail.com>
2017-12-22 23:31:33 +0800

    zan client

--- zanclient.go
+++ zanclient.go
@@ -2,30 +2,33 @@ package uzan
 
 import (
 	"bytes"
-	"cryto/md5"
+	"crypto/md5"
 	"encoding/hex"
-	"sort"
-	"time"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"strings"
-
-    "net/http"
-    "errors"
 )
 
 const (
+	// OpenAPI open api url.
 	OpenAPI = "https://open.youzan.com/api"
-    TimeFormatter = "2006-01-02 15:04:05"
+	// TimeFormatter time formatter.
+	TimeFormatter = "2006-01-02 15:04:05"
 )
 
 // ZanClient zan client for api client.
 type ZanClient struct {
-	ClientId     string
+	ClientID     string
 	ClientSecret string
-	KdtId        int64
+	KdtID        int64
 	AccessToken  string
 }
 
-type Response struct {
+type stringer interface {
+	String() string
 }
 
 func getMd5String(s string) string {
@@ -35,95 +38,92 @@ func getMd5String(s string) string {
 }
 
 func getKeyValue(key interface{}) string {
-
-}
-
-func (c *ZanClient) getSign(secret string, params map[string]interface{}) string {
-	var keys []string
-	for k := range params {
-		keys = append(keys, k)
+	var ret = ""
+	switch valueType := key.(type) {
+	case string:
+		ret = valueType
+	case stringer:
+		ret = valueType.String()
+	default:
+		ret = "Error in value connert"
 	}
-	sort.String(keys)
-
+	return ret
 }
 
-
-// getUrl 生成url.
-func (c *ZanClient) getUrl(method string, version string) string {
-	methodArray := strings.Split(method, '.')
+// getURL get url by youzan method and version.
+func (c *ZanClient) getURL(method string, version string) string {
+	methodArray := strings.Split(method, ".")
 	// 请求action
-	action = methodArray[len(methodArray)-1]
-	service := strings.Join(methodArray[0:len(methodArray)-1], '.')
-	httpUrl := "/" + service + "/" + version + "/" + action 
-	return httpUrl
+	action := methodArray[len(methodArray)-1]
+	service := strings.Join(methodArray[0:len(methodArray)-1], ".")
+	httpURL := "/" + service + "/" + version + "/" + action
+	return httpURL
 }
 
-
+// ZanRequset zan client request.
 func (c *ZanClient) ZanRequset(
-	method string, 
-	params map[string]interface{}, 
+	method string,
+	params map[string]interface{},
 	version string,
 	files map[string]interface{}) ([]byte, error) {
-		url := OpenAPI + "/oauthentry"
-		service := c.getUrl(method, version)
-		var paramsMap map[string]interface{}
+	url := OpenAPI + "/oauthentry"
+	service := c.getURL(method, version)
+	var paramsMap map[string]interface{}
 
-        paramsMap = params
-        paramsMap["access_token"] = c.AccessToken
+	paramsMap = params
+	paramsMap["access_token"] = c.AccessToken
 
-        url += service
+	url += service
 
-        resp, err := c.sendRequest(url, "post", paramsMap, files)
-        defer resp.Body.Close()
+	resp, err := c.sendRequest(url, "post", paramsMap, files)
+	defer resp.Body.Close()
 
-        if err == nil {
-            if resp.StatusCode != http.StatusOK {
-                err = errors.New("http error code: " + string(resp.StatusCode) + " reason: " + resp.Status)
-            }
-        }
+	if err == nil {
+		if resp.StatusCode != http.StatusOK {
+			err = errors.New("http error code: " + string(resp.StatusCode) + " reason: " + resp.Status)
+		}
+	}
 
-        var result []type
-        if err != nil {
-            result, err = ioutil.ReadAll(resp.Body)
-        }
+	var result []byte
+	if err != nil {
+		result, err = ioutil.ReadAll(resp.Body)
+	}
 
-        return result, err
+	return result, err
 
 }
 
 func (c *ZanClient) sendRequest(rawURL string, method string, params map[string]interface{}, files map[string]interface{}) (*http.Response, error) {
-    httpClient := &http.Client{}
+	httpClient := &http.Client{}
 
-    var req *http.Request
-    var err error
-    if "GET" == strings.ToUpper(method) {
-        httpURL := rawURL 
-        if len(params) > 0 {
-            httpURL += "?"
-            for key, value := range params {
-                httpURL += url.QueryEscape(key)
-                httpURL += "="
-                httpURL += url.QueryEscape()
-                httpURL += "&"
-            }
-            httpURL = strings.TrimRight(httpURL, "&")
-        }
-        req, err = http.NewRequest("GET", httpURL, nil)
-    } else if "POST" == strings.ToUpper(method) {
-        jsonData, _ := json.Marshal(params)
-        dataReader := bytes.NewReader(jsonData)
-        req, err = http.NewRequest("POST", rawURL, dataReader)
-        req.Header.Set("Content-Type", "application/json")
-    } else {
-        panic(errors.New("not support method"))
-    }
+	var req *http.Request
+	var err error
+	if "GET" == strings.ToUpper(method) {
+		httpURL := rawURL
+		if len(params) > 0 {
+			httpURL += "?"
+			for key, value := range params {
+				httpURL += url.QueryEscape(key)
+				httpURL += "="
+				httpURL += url.QueryEscape(getKeyValue(value))
+				httpURL += "&"
+			}
+			httpURL = strings.TrimRight(httpURL, "&")
+		}
+		req, err = http.NewRequest("GET", httpURL, nil)
+	} else if "POST" == strings.ToUpper(method) {
+		jsonData, _ := json.Marshal(params)
+		dataReader := bytes.NewReader(jsonData)
+		req, err = http.NewRequest("POST", rawURL, dataReader)
+		req.Header.Set("Content-Type", "application/json")
+	} else {
+		panic(errors.New("not support method"))
+	}
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    req.Header.Add("User-Agent", "X-UZan-Client")
-    return httpClient.Do(req)
+	req.Header.Add("User-Agent", "X-UZan-Client")
+	return httpClient.Do(req)
 }
-
-
